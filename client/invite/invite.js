03a9fe59ff9bb833fff66a7d6518f947b80e8b7c
sarasate <luis@superlumen.io>
2018-02-22 17:07:29 +0100

    Fix typos

--- client/invite/invite.js
+++ client/invite/invite.js
@@ -1,16 +1,16 @@
 Template.invite.events({
-	'click .email-submit-button': function(event) {
-		event.preventDefault();
-		const thisCase = Session.get('case');
-		const thisUser = Session.get('user');
-		console.log(thisUser);
-		FlowRouter.go('/modules/' + thisCase + '/' + thisUser);
-	}
+  "click .email-submit-button": function(event) {
+    event.preventDefault();
+    const thisCase = Session.get("case");
+    const thisUser = Session.get("user");
+    console.log(thisUser);
+    FlowRouter.go("/modules/" + thisCase + "/" + thisUser);
+  }
 });
 
 Template.layout.onRendered(function() {
-	const thisCase = FlowRouter.getParam('case');
-	const thisUser = FlowRouter.getParam('user');
-	Session.set('user', thisUser);
-	Session.set('case', thisCase);
+  const thisCase = FlowRouter.getParam("case");
+  const thisUser = FlowRouter.getParam("user");
+  Session.set("user", thisUser);
+  Session.set("case", thisCase);
 });
