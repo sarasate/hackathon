cf399871065a0a60646d5abd51e5875db162a1b6
sarasate <luis@superlumen.io>
2018-02-22 12:04:10 +0100

    Cleanup

--- client/main.js
+++ client/main.js
@@ -3,18 +3,18 @@ import { ReactiveVar } from "meteor/reactive-var";
 
 import "./main.html";
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.index.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.index.helpers({
   counter() {
     return Template.instance().counter.get();
   }
 });
 
-Template.hello.events({
+Template.index.events({
   "click button"(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
