ad924a4fe539e6ce44db54e6a4aee2c47b0b74e4
sarasate <luis@superlumen.io>
2018-02-23 11:53:01 +0100

    Add score counters

--- client/scoring/scoring.template.js
+++ client/scoring/scoring.template.js
@@ -1,5 +1,14 @@
 import { get } from "lodash";
 
+Template.scoring.onRendered(function() {
+  this.autorun(function() {
+    const maxCount = Questions.find().fetch().length * 3;
+    const scoreCount = Questions.find().fetch().length;
+    Session.set("maxCount", maxCount);
+    Session.set("scoreCount", scoreCount);
+  });
+});
+
 Template.scoring.helpers({
   questions: () => {
     return (questionsArray = Questions.find().fetch());
@@ -11,21 +20,46 @@ Template.scoring.helpers({
   weight: _id => {
     const answer = Answers.findOne({ questionId: _id });
     return get(answer, "weight");
+  },
+  answerId: _id => {
+    const answer = Answers.findOne({ questionId: _id });
+    return get(answer, "_id");
+  },
+  maxCount: () => {
+    return Session.get("maxCount");
+  },
+  scoreCount: () => {
+    return Session.get("scoreCount");
   }
 });
 
 Template.scoring.events({
   "click .plus-button": event => {
-    console.log(event);
-  },
-  'click .scoring-confirm': function(event) {
-		event.preventDefault();
-		$('.waiting-for-other-party').addClass('show');
-		const thisCase = Session.get('case');
-		const thisUser = Session.get('user');
-		setTimeout(function() {
-					FlowRouter.go('/proposal/' + thisCase + '/' + thisUser);
-		}, 10000);
+    const answerId = event.target.id;
+    let scoreCount = Session.get("scoreCount");
+    const maxCount = Session.get("maxCount");
 
-	}
+    if (scoreCount === maxCount) return;
+
+    Session.set("scoreCount", scoreCount + 1);
+    Answers.update(answerId, { $inc: { weight: 1 } });
+  },
+  "click .minus-button": event => {
+    const answerId = event.target.id;
+    let scoreCount = Session.get("scoreCount");
+
+    if (scoreCount <= 0) return;
+
+    Session.set("scoreCount", scoreCount - 1);
+    Answers.update(answerId, { $inc: { weight: -1 } });
+  },
+  "click .scoring-confirm": function(event) {
+    event.preventDefault();
+    $(".waiting-for-other-party").addClass("show");
+    const thisCase = Session.get("case");
+    const thisUser = Session.get("user");
+    setTimeout(function() {
+      FlowRouter.go("/proposal/" + thisCase + "/" + thisUser);
+    }, 10000);
+  }
 });
