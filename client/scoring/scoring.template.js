7091e74b29ad6dcdfb1c0501a15fb73e7603d0ad
sarasate <luis@superlumen.io>
2018-02-23 10:54:39 +0100

    Scoring events

--- client/scoring/scoring.template.js
+++ client/scoring/scoring.template.js
@@ -7,5 +7,15 @@ Template.scoring.helpers({
   answer: _id => {
     const answer = Answers.findOne({ questionId: _id });
     return get(answer, "value");
+  },
+  weight: _id => {
+    const answer = Answers.findOne({ questionId: _id });
+    return get(answer, "weight");
+  }
+});
+
+Template.scoring.events({
+  "click .plus-button": event => {
+    console.log(event);
   }
 });
