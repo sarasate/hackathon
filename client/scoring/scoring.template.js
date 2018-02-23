ec78f0ca1ae216127b6f0eff988971a53f9fd300
sarasate <luis@superlumen.io>
2018-02-23 10:02:58 +0100

    Show answer in question

--- client/scoring/scoring.template.js
+++ client/scoring/scoring.template.js
@@ -1,8 +1,11 @@
+import { get } from "lodash";
+
 Template.scoring.helpers({
-    questions: () => { 
-      return questionsArray = Questions.find().fetch();
-    },
-    answer: () => {
-        return 'Das ist eine Antwort';
-    }
-  });
+  questions: () => {
+    return (questionsArray = Questions.find().fetch());
+  },
+  answer: _id => {
+    const answer = Answers.findOne({ questionId: _id });
+    return get(answer, "value");
+  }
+});
