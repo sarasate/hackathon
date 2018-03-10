8314ea16d04e52a9097515a9d7d4dbd273e27fc8
sarasate <luis@superlumen.io>
2018-03-10 11:48:30 +0100

    Improve invite page

--- client/invite/invite.js
+++ client/invite/invite.js
@@ -1,15 +1,28 @@
 Template.invite.events({
-	'click .email-submit-button': function(event) {
-		event.preventDefault();
-		const thisCase = Session.get('case');
-		const thisUser = Session.get('user');
-		FlowRouter.go('/modules/' + thisCase + '/' + thisUser);
-	}
+  "submit .invite-form": function(event) {
+    event.preventDefault();
+    // Fetch form field.
+    const target = event.target;
+    console.log(target);
+    const issue = {
+      user1: target.username1.value,
+      email1: target.email1.value,
+      user2: target.username2.value,
+      email2: target.email2.value
+    };
+
+    const issueId = Cases.insert(issue);
+
+    Session.set("user", issue.email1);
+    Session.set("case", issueId);
+
+    const thisCase = Session.get("case");
+    const thisUser = Session.get("user");
+    FlowRouter.go("/modules/" + thisCase + "/" + thisUser);
+  }
 });
 
 Template.layout.onRendered(function() {
   const thisCase = FlowRouter.getParam("case");
   const thisUser = FlowRouter.getParam("user");
-  Session.set("user", thisUser);
-  Session.set("case", thisCase);
 });
