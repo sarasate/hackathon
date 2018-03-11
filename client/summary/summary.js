bf0dc51d2575a6d71694855d537f0886b320cf1e
Johannes Wagner <wagner@lawlift.de>
2018-03-11 12:40:09 +0100

    Add alert messages in summary.

--- client/summary/summary.js
+++ client/summary/summary.js
@@ -3,3 +3,12 @@ Template.summary.helpers({
     return Math.round(Math.random() * 19 + 70);
   }
 });
+
+Template.summary.events({
+  "click .retry-button": function () {
+    alert('The remaining points of conflict will be proposed again in different combinations.')
+  }, 
+  "click .consult-button": function () {
+    alert('Here you will find further consultation possibilities (specialised lawyers / mediators).')
+  }, 
+}) 
